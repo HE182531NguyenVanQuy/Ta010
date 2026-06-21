e39dcb04db6f7ecfb64e74ea70ac56ff943f601e
NguyenVanQuy <vickquy1312004@gmail.com>
2026-06-21 17:24:36 +0700

    21/06

--- src/app/services/dashboard-admin.service.ts
+++ src/app/services/dashboard-admin.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, firstValueFrom } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 import { ApiResponse } from '../models/user-admin.model';
 import { 
   DashboardStatsDto, 
