da8ec5fa58e13eda92514f05d9912ea8ebc7066a
NguyenVanQuy <vickquy1312004@gmail.com>
2026-06-22 02:13:01 +0700

    22/06

--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Inject, PLATFORM_ID, inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { HubConnection, HubConnectionBuilder, HttpTransportType, LogLevel } from '@microsoft/signalr';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 import { AuthService } from './auth.service';
 import { NotificationService } from './notification.service';
 
