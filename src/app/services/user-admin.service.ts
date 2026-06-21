5b8bc320367ccc22e1c911cd9d3a2187abe1860a
NguyenVanQuy <vickquy1312004@gmail.com>
2026-06-21 18:13:28 +0700

    21/06

--- src/app/services/user-admin.service.ts
+++ src/app/services/user-admin.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, firstValueFrom } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 import { ApiResponse, UserPagedResponse, UserDetailDto } from '../models/user-admin.model';
 
 @Injectable({
