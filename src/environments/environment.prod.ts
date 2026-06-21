a6c3593e98c7c218488da7879a47c860c8f8ac7f
NguyenVanQuy <vickquy1312004@gmail.com>
2026-06-21 17:22:25 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://tao10-backend.onrender.com/api'
-    apiUrl: 'http://localhost:5185/api' 
+  apiUrl: 'https://tao10-backend.onrender.com/api'
 };
