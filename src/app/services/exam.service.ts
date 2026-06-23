bab9426a53effd735eb7135a4da2cecfff89ba6e
NguyenVanQuy <vickquy1312004@gmail.com>
2026-06-24 03:04:39 +0700

    24/06

--- src/app/services/exam.service.ts
+++ src/app/services/exam.service.ts
@@ -1,5 +1,5 @@
 ﻿import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 const API_BASE = (environment.apiUrl ?? 'https://localhost:7103/api').replace(/\/+$/g, '');
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5185/api'
+  apiUrl: 'https://tao10-backend.onrender.com/api'
 };
